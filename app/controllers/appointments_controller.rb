dab92f99ae35a799f5e5751157e36c43336bab55
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-17 10:46:23 +0300

    destroy route to nullify an appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -9,4 +9,10 @@ class AppointmentsController < ApplicationController
     render json: appointment, status: :ok
   end
 
+  def destroy
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    head :no_content
+  end
+
 end
