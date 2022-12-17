6999a9533a6e11efd9073c1c4f4c4b02b4d383ce
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-17 10:45:32 +0300

    show route to return a single appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,4 +4,9 @@ class AppointmentsController < ApplicationController
     render json: Appointment.all.order(:start_date), status: :ok
   end
 
+  def show
+    appointment = Appointment.find(params[:id])
+    render json: appointment, status: :ok
+  end
+
 end
