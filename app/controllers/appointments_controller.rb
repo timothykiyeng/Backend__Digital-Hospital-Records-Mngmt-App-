29046227b891c1b1a07b2e7a07bfbaeba3e114b7
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-17 11:06:40 +0300

    find_appointment - helper method - reduces repetition

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,7 +5,7 @@ class AppointmentsController < ApplicationController
   end
 
   def show
-    appointment = Appointment.find(params[:id])
+    appointment = find_appointment
     render json: appointment, status: :ok
   end
 
@@ -14,17 +14,21 @@ class AppointmentsController < ApplicationController
   end
 
   def update
-    appointment = Appointment.find(params[:id])
+    appointment = find_appointment
     render json: appointment.update!(appointment_params), status: :ok
   end
 
   def destroy
-    appointment = Appointment.find(params[:id])
+    appointment = find_appointment
     appointment.destroy
     head :no_content
   end
 
   private
+
+  def find_appointment
+    Appointment.find(params[:id])
+  end
 
   def appointment_params
     params.permit(:id, :title, :start_date, :end_date, :notes, :patient_id, :doctor_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :medical_histories
-  resources :departments
+  resources :medical_histories, only: [:index]
+  resources :departments, only: [:index]
   resources :appointments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
