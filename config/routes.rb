feaea272aee974053d6b238b325b783746e63627
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-19 13:51:04 +0300

    ft: routes - creates the RESTful routes for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :patients
+  resources :doctors, only: [:index, :show]
+  resources :appointments
+  resources :medical_histories, only: [:index]
+  resources :departments, only: [:index]
 end
